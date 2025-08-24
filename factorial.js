da81d65648c4bac8dcea1589639c477d48ed4424
Sivachandran <sivachandranproff@gmail.com>
2025-08-24 15:59:46 +0530

    Create factorial.js

---
+++ factorial.js
@@ -0,0 +1,21 @@
+
+// factorial.js
+function factorial(n) {
+    let fact = 1;
+    for (let i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+console.log(factorial(5)); // 120
+
+function factorial(n) {
+    let fact = 1;
+    for (let i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+console.log(factorial(5)); // 120
