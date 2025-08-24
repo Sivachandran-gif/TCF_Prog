5eaa2b9355afa2912ae4b1012179e052d01444eb
Sivachandran <sivachandranproff@gmail.com>
2025-08-24 16:00:07 +0530

    Create object.js

---
+++ object.js
@@ -0,0 +1,12 @@
+
+// object.js
+
+// Object literal
+const person = {
+    name: "Robert",
+    age: 30,
+    city: "London"
+};
+
+console.log(person.name);  // Robert
+console.log(person["age"]); // 30
